8642264933f697fc5894b52a7be884944c06a1aa
VISHAL <vishalkumar3474@gmail.com>
2022-08-23 21:27:33 +0530

    Responsive

--- src/Pricing.jsx
+++ src/Pricing.jsx
@@ -3,7 +3,7 @@ import PricingCard from "./PricingCard";
 
 function Pricing() {
   return (
-    <div className="bg-gray-100 lg:mb-10 lg:mt-5 ">
+    <div className="bg-gray-100 mb-10 mt-5 ">
         <div className="lg:text-center lg:ml-72 lg:mr-72"> 
       <h1 className="text-3xl mt-10">Pricing plan</h1>
       <h1 className="text-sm text-gray-600 mt-10 mb-10">
